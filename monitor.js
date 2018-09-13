083a5cfcbe3805f6b99a7d302771dff39d3d1977
rbortnikas <bortnro@gmail.com>
2018-09-13 20:27:40 +0100

    table remake

--- monitor.js
+++ monitor.js
@@ -19,11 +19,7 @@ class Monitor {
     };
 
     calculate(size, ratio) {
-      // let height = 10;
       let splitRatio = this.ratio.split("-");
-      // console.log(splitRatio);
-      // console.log("0 -- " + splitRatio[0]);
-      // console.log("0 -- " + splitRatio[1]);
       let height = (size/((1+(splitRatio[0]/splitRatio[1])**2)**0.5))*pxMultiplier;
       let width = (size*splitRatio[0]/splitRatio[1])/((1+(splitRatio[0]/splitRatio[1])**2)**0.5)*pxMultiplier;
 
@@ -32,10 +28,6 @@ class Monitor {
     };
 
     update(id) {
-      // console.log(obj[id].height);
-      // obj[id].height = obj[id].calculate(obj[id].size, obj[id].ratio).height;
-      // console.log(obj[id].height);
-      // obj[id].width = obj[id].calculate(obj[id].size, obj[id].ratio).width;
       $("#" + this._id).closest(".monitor").css("width", this.width);
       $("#" + this._id).closest(".monitor").css("height", this.height);
     };
@@ -52,33 +44,36 @@ class Monitor {
       var width = obj[id].height;
       obj[id].height = height;
       obj[id].width = width;
-      // console.log(obj[id]);
     }
 }
 
 
 //Create monitor
-$("#addMonitor").on("click", function(){
+$(".sideButton").on("click", function(){
   i++;
   obj[i] = new Monitor();
   console.log("Monitor object created: " + i);
   $("#monitorArea").append("<div class=\"monitor\" id=\"" + obj[i]._id + "\"><p class=\"monitorNumber\"></p></div>");
-  const table = "<td id=\"dropdownCell\" class=\"dropdownCell\"><input id=\"input\" type=\"number\" name=\"Diagonal\" class=\"no-spin\" value=\"24\"></td><td class=\"dropdownCell\"><select class=\"ui fluid normal dropdown ratioSelect\"><option value=\"16-9\">16:9</option><option value=\"16-10\">16:10</option><option value=\"21-9\">21:9</option><option value=\"4-3\">4:3</option></select></td><td class=\"rotate\"><i class=\"large redo alternate icon\"></i>Rotate</td><td class=\"remove\"><i class=\"large times icon\"></i>Remove</td></tr>"
-  // console.log(obj[i].height);
-  // console.log(obj[i].ratio);
-  // console.log(obj[i].size);
+  const table = "<td id=\"dropdownCell\" class=\"dropdownCell\"><div class=\"ui fluid input\"><input id=\"input\" type=\"number\" name=\"Diagonal\" class=\"no-spin\" value=\"24\"><\/div><\/td>\r\n    <td class=\"dropdownCell\">\r\n      <select class=\"ui fluid normal dropdown button ratioSelect\"><option value=\"16-9\">16:9<\/option>\r\n        <option value=\"16-10\">16:10<\/option>\r\n        <option value=\"21-9\">21:9<\/option>\r\n        <option value=\"4-3\">4:3<\/option>\r\n      <\/select>\r\n    <\/td>\r\n    <td class=\"rotate\"><i class=\"large redo alternate icon\"><\/i>Rotate<\/td>\r\n    <td class=\"remove\"><i class=\"large times icon\"><\/i>Remove<\/td>\r\n  <\/tr>"
+
+
+
+
+
+
+
+
   obj[i].height = obj[i].calculate(obj[i].size, obj[i].ratio).height;
-  // console.log(obj[i].height);
   obj[i].width = obj[i].calculate(obj[i].size, obj[i].ratio).width;
   obj[i].update(i);
-	$("#table").append("<tr id=" + obj[i]._id + "><td colspan=\"2\">" + obj[i]._id + "</td>" + table);
- 	$(".monitor:last").css("left", 49 + obj[i]._id + "%").css("top", 14 + obj[i]._id + "%");
+	$("#table").append("<tr id=" + obj[i]._id + ">\r\n    <td class=\"rowId\">" + obj[i]._id + "<\/td>\r\n" + table);
+ 	$(".monitor:last").css("left", 40 + obj[i]._id + "%").css("top", 14 + obj[i]._id + "%");
  	$(".monitor p:last").text(obj[i]._id);
  	events();
 });
 
 //triger click to create first monitor
-$("#addMonitor").trigger("click");
+$(".sideButton").trigger("click");
 
 function events(){
   $(".ratioSelect").change(function(){
@@ -115,8 +110,14 @@ function events(){
       console.log("rotated");
     });
 
-    //remove offset
-    $(".monitor").on("mouseenter", function(){
-      $(this).css("transform", "translateX(0%)");
-    });
+    //Bring monitor forward on drag
+    $( ".monitor" ).mousedown(function() {
+      $(".monitor").css("z-index", "0");
+      $(this).css("z-index", "1")
+    })
+      .mouseup(function(){$(this).css("z-index", "0")});
+
+    //Semantic UI dropdown
+    $('.ui.dropdown')
+      .dropdown();
 };
